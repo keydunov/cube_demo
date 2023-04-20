6675f6806c82da55a5ee778c06d899bb24e99379
system <system@cubecloud.dev>
2023-04-20 19:13:01 +0000

    metabase test

--- cube.js
+++ cube.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+  queryRewrite: (query, { securityContext }) => {
+    if (securityContext.categoryId) {
+      query.filters.push({
+        member: 'Products.categoryId',
+        operator: 'equals',
+        values: [securityContext.categoryId],
+      });
+    }
+    return query;
+  },
+};
