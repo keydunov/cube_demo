bc12c240d9948a2f38e6c13d0d43eb346df9ecec
pavel <pavel@cube.dev>
2023-04-28 12:05:53 +0000

    Batch update files

--- cube.js
+++ cube.js
@@ -12,12 +12,11 @@ module.exports = {
   semanticLayerSync: () => {
     return [
       {
-        type: 'preset',
+        type: 'metabase',
         config: {
-          api_secret: 'b91859e67b3bf056e2329820002c556a0a43861502e88af66a608e936006de34',
-          workspace_slug: 'e5ae36b9',
-          api_token: '94b5d1d0-2752-472c-8b9b-d724931bc0fc',
-          workspace_region: 'us1a'
+          url: 'https://cube-dev-partner.metabaseapp.com',
+          user: 'artyom+cubeserviceaccount@cube.dev',
+          password: 'qdy4FPT@xde2ehn4pqj'
         }
       }
     ]
