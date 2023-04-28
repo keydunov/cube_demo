cd11f238568c33092ec8654674489f5b6864e150
pavel <pavel@cube.dev>
2023-04-28 11:20:34 +0000

    Batch update files

--- cube.js
+++ cube.js
@@ -9,4 +9,17 @@ module.exports = {
     }
     return query;
   },
+  semanticLayerSync: () => {
+    return [
+      {
+        type: 'preset',
+        config: {
+          api_secret: 'b91859e67b3bf056e2329820002c556a0a43861502e88af66a608e936006de34',
+          workspace_slug: 'e5ae36b9',
+          api_token: '94b5d1d0-2752-472c-8b9b-d724931bc0fc',
+          workspace_region: 'us1a'
+        }
+      }
+    ]
+  }
 };
