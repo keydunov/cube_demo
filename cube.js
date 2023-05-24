1dec29b24541eb590e7c36265674a4f3c3aad60f
system <system@cubecloud.dev>
2023-05-24 06:49:00 +0000

    bad preset test

--- cube.js
+++ cube.js
@@ -1,24 +1,32 @@
 module.exports = {
-  queryRewrite: (query, { securityContext }) => {
+  queryRewrite: (query, {
+    securityContext
+  }) => {
     if (securityContext.categoryId) {
       query.filters.push({
         member: 'Products.categoryId',
         operator: 'equals',
-        values: [securityContext.categoryId],
+        values: [securityContext.categoryId]
       });
     }
     return query;
   },
   semanticLayerSync: () => {
-    return [
-      {
-        type: 'metabase',
-        config: {
-          url: 'https://cube-dev-partner.metabaseapp.com',
-          user: 'artyom+cubeserviceaccount@cube.dev',
-          password: 'qdy4FPT@xde2ehn4pqj'
-        }
+    return [{
+      type: 'metabase',
+      config: {
+        url: 'https://cube-dev-partner.metabaseapp.com',
+        user: 'artyom+cubeserviceaccount@cube.dev',
+        password: 'qdy4FPT@xde2ehn4pqj'
       }
-    ]
+    }, {
+      type: "preset",
+      name: "fff",
+      config: {
+        api_token: "fff",
+        api_secret: "ff",
+        workspace_url: "fff"
+      }
+    }];
   }
 };
