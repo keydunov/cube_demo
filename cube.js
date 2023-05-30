fe967f09242078f9c85a63a4355a0c3b61b40058
artyom <artyom@cube.dev>
2023-05-30 15:31:53 +0000

    Add Preset SLS

--- cube.js
+++ cube.js
@@ -1,2 +1,13 @@
 module.exports = {
+  semanticLayerSync: () => {
+    return [{
+      type: "preset",
+      name: "Preset Sync",
+      config: {
+        api_token: "fcda2862-ae7e-41c4-9d30-6a80a3fe0a03",
+        api_secret: "6ab38d04215d27f7bc934c0be3e96d9e8a155e9a1192198c07b17064c63e0fb8",
+        workspace_url: "e5ae36b9.us1a.app.preset.io"
+      }
+    }];
+  }
 };
