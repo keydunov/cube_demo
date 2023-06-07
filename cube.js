e0b1b301670ad13d000a6c72ae69ef44ceaee077
artyom <artyom@cube.dev>
2023-06-07 05:25:14 +0000

    Update semantic layer config

--- cube.js
+++ cube.js
@@ -4,9 +4,9 @@ module.exports = {
       type: "preset",
       name: "Preset Sync",
       config: {
-        api_token: "fcda2862-ae7e-41c4-9d30-6a80a3fe0a03",
-        api_secret: "6ab38d04215d27f7bc934c0be3e96d9e8a155e9a1192198c07b17064c63e0fb8",
-        workspace_url: "e5ae36b9.us1a.app.preset.io"
+        api_token: process.env.PRESET_API_TOKEN,
+        api_secret: process.env.PRESET_API_SECRET,
+        workspace_url: process.env.PRESET_WORKSPACE_URL
       }
     }];
   }
