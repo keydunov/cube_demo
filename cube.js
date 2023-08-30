376fd4654c362bb9e51d5a5c5f5beeae129b07a7
Artyom Keydunov <artyom@cube.dev>
2023-08-29 22:06:09 -0700

    Revert "Multi-tenancy example"
    This reverts commit 0f5a319c0146cd7676ae1136262e6504c097a08f.

--- cube.js
+++ cube.js
@@ -1,45 +1,12 @@
 module.exports = {
-  // Create a new appId for each table
-  // https://cube.dev/docs/reference/configuration/config#contexttoappid
-  contextToAppId: ({ securityContext }) => {
-    return securityContext.table;
-  },
- 
-  // Enforce a default value for `table` if one is not provided in the security context
-  // https://cube.dev/docs/reference/configuration/config#extendcontext
-  extendContext: ({ securityContext }) => {
-    if (!securityContext.team) {
-      securityContext.table = "ORDERS";
-    }
- 
-    return {
-      securityContext,
-    };
-  },
- 
-  // Here we create a new security context from Metabase SQL API call
-  // Payload is being passed as JSON string all the way from python app through Metabase
-  checkSqlAuth: (query, username) => {
-    const payload = JSON.parse(payload)
-    const securityContext = {
-      table: payload.table,
-    };
- 
-    return {
-      password: process.env.CUBEJS_SQL_PASSWORD,
-      securityContext: securityContext,
-    };
-  },
-
   semanticLayerSync: () => {
     return [{
-      type: "metabase",
-      name: "Metabase Sync",
+      type: "preset",
+      name: "Preset Sync",
       config: {
-        database: "metabase-jtw-dynamic-data-model",
-        user: process.env.METABASE_SLS_USER,
-        password: process.env.METABASE_SLS_PASSWORD,
-        url: process.env.METABASE_SLS_URL
+        api_token: process.env.PRESET_API_TOKEN,
+        api_secret: process.env.PRESET_API_SECRET,
+        workspace_url: process.env.PRESET_WORKSPACE_URL
       }
     }];
   }
