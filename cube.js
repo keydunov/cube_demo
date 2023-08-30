76f517d971a12a22b47ad404b512d2122e600591
artyom <artyom@cube.dev>
2023-08-30 03:35:51 +0000

    Let SQL superuser connect

--- cube.js
+++ cube.js
@@ -2,14 +2,14 @@ module.exports = {
   // Create a new appId for each table
   // https://cube.dev/docs/reference/configuration/config#contexttoappid
   contextToAppId: ({ securityContext }) => {
-    return securityContext.table;
+    return securityContext.table
   },
  
   // Enforce a default value for `table` if one is not provided in the security context
   // https://cube.dev/docs/reference/configuration/config#extendcontext
-  extendContext: ({ securityContext }) => {
-    if (!securityContext.team) {
-      securityContext.table = "ORDERS";
+  extendContext: ({ securityContext = {} }) => {
+    if (!securityContext.table) {
+      securityContext.table = "ORDERS"
     }
  
     return {
@@ -20,10 +20,12 @@ module.exports = {
   // Here we create a new security context from Metabase SQL API call
   // Payload is being passed as JSON string all the way from python app through Metabase
   checkSqlAuth: (query, username) => {
-    const payload = JSON.parse(payload)
-    const securityContext = {
-      table: payload.table,
-    };
+    let securityContext = {}
+    if (username != "cube") {
+      const payload = JSON.parse(payload)
+      securityContext.table = payload.table
+    }
+    
  
     return {
       password: process.env.CUBEJS_SQL_PASSWORD,
@@ -35,8 +37,8 @@ module.exports = {
     return [{
       type: "metabase",
       name: "Metabase Sync",
+      database: "metabase-jwt-dynamic-data-model",
       config: {
-        database: "metabase-jtw-dynamic-data-model",
         user: process.env.METABASE_SLS_USER,
         password: process.env.METABASE_SLS_PASSWORD,
         url: process.env.METABASE_SLS_URL
