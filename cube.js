0f5a319c0146cd7676ae1136262e6504c097a08f
artyom <artyom@cube.dev>
2023-08-30 03:18:52 +0000

    Multi-tenancy example

--- cube.js
+++ cube.js
@@ -1,12 +1,45 @@
 module.exports = {
+  // Create a new appId for each table
+  // https://cube.dev/docs/reference/configuration/config#contexttoappid
+  contextToAppId: ({ securityContext }) => {
+    return securityContext.table;
+  },
+ 
+  // Enforce a default value for `table` if one is not provided in the security context
+  // https://cube.dev/docs/reference/configuration/config#extendcontext
+  extendContext: ({ securityContext }) => {
+    if (!securityContext.team) {
+      securityContext.table = "ORDERS";
+    }
+ 
+    return {
+      securityContext,
+    };
+  },
+ 
+  // Here we create a new security context from Metabase SQL API call
+  // Payload is being passed as JSON string all the way from python app through Metabase
+  checkSqlAuth: (query, username) => {
+    const payload = JSON.parse(payload)
+    const securityContext = {
+      table: payload.table,
+    };
+ 
+    return {
+      password: process.env.CUBEJS_SQL_PASSWORD,
+      securityContext: securityContext,
+    };
+  },
+
   semanticLayerSync: () => {
     return [{
-      type: "preset",
-      name: "Preset Sync",
+      type: "metabase",
+      name: "Metabase Sync",
       config: {
-        api_token: process.env.PRESET_API_TOKEN,
-        api_secret: process.env.PRESET_API_SECRET,
-        workspace_url: process.env.PRESET_WORKSPACE_URL
+        database: "metabase-jtw-dynamic-data-model",
+        user: process.env.METABASE_SLS_USER,
+        password: process.env.METABASE_SLS_PASSWORD,
+        url: process.env.METABASE_SLS_URL
       }
     }];
   }
